fe0ac8468f29a2f452008da5e93cd1f4b9977bc7
Kristian Haugene <kristian.haugene@capgemini.com>
2012-10-26 00:24:58 +0200

    found a nice little open source json call helper: Rusty! We have contact...

---
+++ application/src/main/java/no/ruter/app/repository/RuterRepository.java
@@ -0,0 +1,27 @@
+package no.ruter.app.repository;
+
+import java.io.IOException;
+
+import us.monoid.web.Resty;
+import us.monoid.web.TextResource;
+
+public class RuterRepository {
+	
+	public static void main(String[] args) {
+		doIt();
+	}
+
+	// It does it
+	public static void doIt(){
+		
+		try {
+			TextResource response = new Resty().text("http://api-test.trafikanten.no/Place/FindMatches/nationaltheateret");
+			System.out.println(response.toString());
+			
+			
+		} catch (IOException e) {
+			System.out.println("I failed");
+		}
+	}
+	
+}
