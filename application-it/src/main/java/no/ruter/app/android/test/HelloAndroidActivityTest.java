a48bfc8fffe247c73a54971d02d1885b9767688b
Daniel Nordstroem Carlsen <daniel.n.carlsen@gmail.com>
2012-11-01 19:49:04 +0100

    Added tabs with swipe-navigation. Remember to add android.support.v4

--- application-it/src/main/java/no/ruter/app/android/test/HelloAndroidActivityTest.java
+++ application-it/src/main/java/no/ruter/app/android/test/HelloAndroidActivityTest.java
@@ -3,14 +3,14 @@ package no.ruter.app.android.test;
 import android.test.ActivityInstrumentationTestCase2;
 import no.ruter.app.android.*;
 
-public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<MainViewActivity> {
 
     public HelloAndroidActivityTest() {
-        super(HelloAndroidActivity.class);
+        super(MainViewActivity.class);
     }
 
     public void testActivity() {
-        HelloAndroidActivity activity = getActivity();
+        MainViewActivity activity = getActivity();
         assertNotNull(activity);
     }
 }
