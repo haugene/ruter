025c6ea1961fd32b55f87ced00e9a3704e259f94
Kristian Haugene <kristian.haugene@capgemini.com>
2012-11-15 22:46:39 +0100

    added name to platform

--- application/src/main/java/no/ruter/app/domain/Platform.java
+++ application/src/main/java/no/ruter/app/domain/Platform.java
@@ -1,5 +1,33 @@
 package no.ruter.app.domain;
 
+/**
+ * Represents a platform
+ * 
+ * @author Kristian
+ * 
+ */
 public class Platform {
+
+	/*
+	 * Member variables
+	 */
+	private String name;
+
+	/**
+	 * Parameterized constructor
+	 * 
+	 * @param name
+	 *            platform name
+	 */
+	public Platform(String name) {
+		this.name = name;
+	}
+
+	/*
+	 * Getters and setters
+	 */
+	public String getName() {
+		return name;
+	}
 
 }
