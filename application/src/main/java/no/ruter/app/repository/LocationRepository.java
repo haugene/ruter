7ab9935785174e1effa69c152185f206f753a543
Kristian Haugene <kristian.haugene@capgemini.com>
2012-12-11 22:59:54 +0100

    created observer interfaces for location updates. Implemented through repository, needs service.

--- application/src/main/java/no/ruter/app/repository/LocationRepository.java
+++ application/src/main/java/no/ruter/app/repository/LocationRepository.java
@@ -1,6 +1,7 @@
 package no.ruter.app.repository;
 
 import no.ruter.app.exception.RepositoryException;
+import no.ruter.app.observers.LocationObserver;
 import android.content.Context;
 import android.location.Location;
 
@@ -22,5 +23,17 @@ public interface LocationRepository {
 	 * @throws RepositoryException 
 	 */
 	public Location getCurrentLocation(Context context) throws RepositoryException;
+	
+	/**
+	 * This method lets you register a {@link LocationObserver} that will be
+	 * notified each time a better location is found. <br />
+	 * NOTE: After a certain time, we will stop listening for updates and a new
+	 * observer will need to be registered to initiate new location searching
+	 * 
+	 * @param observer
+	 * @param context
+	 * @throws RepositoryException
+	 */
+	public void registerLocationObserver(LocationObserver observer, Context context) throws RepositoryException;
 
 }
